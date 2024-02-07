7a3f20dd268a2bb2f7d359cd9c7f3a98f7e6c6ca
Anaphee01 <anaphee007@gmail.com>
2024-02-07 22:38:29 +0700

    Update ZOOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,6 +22,50 @@ const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
 
+//popup
+const images = [...document.querySelectorAll('.image')];
+
+const popup = document.querySelector('.popup');
+const closeBtn = document.querySelector('.close-btn');
+const imageName = document.querySelector('.image-name');
+const largeImage = document.querySelector('.large-image');
+const imageIndex = document.querySelector('.index');
+const leftArrow = document.querySelector('.left-arrow');
+const rightArrow = document.querySelector('.right-arrow');
+
+let index = 0; // will track our current image;
+
+images.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        updateImage(i);
+        popup.classList.toggle('active');
+    })
+})
+
+const updateImage = (i) => {
+    let path = `assets/images/port-${i+1}.png`;
+    largeImage.src = path;
+    imageName.innerHTML = path;
+    imageIndex.innerHTML = `0${i+1}`;
+    index = i;
+}
+
+closeBtn.addEventListener('click', () => {
+    popup.classList.toggle('active');
+})
+
+leftArrow.addEventListener('click', () => {
+    if(index > 0){
+        updateImage(index - 1);
+    }
+})
+
+rightArrow.addEventListener('click', () => {
+    if(index < images.length - 1){
+        updateImage(index + 1);
+    }
+})
+
 // modal variable
 const modalImg = document.querySelector("[data-modal-img]");
 const modalTitle = document.querySelector("[data-modal-title]");
